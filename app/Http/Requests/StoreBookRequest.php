239525e939bdf3c123eabfbab1733d468cb41bf4
Tatsuya Yamasaki <zakkikki1024@yahoo.co.jp>
2026-08-04 19:36:39 +0900

    feat: 書籍CRUD実装 #5

---
+++ app/Http/Requests/StoreBookRequest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreBookRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => ['required', 'string', 'max:255'],
+            'author' => ['required', 'string', 'max:255'],
+            'isbn' => ['required', 'digits:13', 'unique:books,isbn'],
+            'publication_date' => ['required', 'date'],
+            'description' => ['nullable', 'string', 'max:1000'],
+            'image_url' => ['nullable', 'url', 'max:255'],
+            'genre_ids' => ['required', 'array', 'min:1'],
+            'genre_ids.*' => ['integer', 'exists:genres,id'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'title.required' => 'タイトルを入力してください。',
+            'title.max' => 'タイトルは255文字以内で入力してください。',
+
+            'author.required' => '著者を入力してください。',
+            'author.max' => '著者名は255文字以内で入力してください。',
+
+            'isbn.required' => '13桁のISBNを入力してください。',
+            'isbn.digits' => '13桁のISBNを入力してください。',
+            'isbn.unique' => 'そのISBNは既に登録されています。',
+
+            'publication_date.required' => '出版日を選択してください。',
+
+            'description.max' => '説明は1000文字以内で入力してください。',
+
+            'image_url.url' => '画像URLは正しいURL形式で入力してください。',
+            'image_url.max' => '画像URLは255文字以内で入力してください。',
+
+            'genres.required' => 'ジャンルを1つ以上選択してください。',
+        ];
+    }
+}
