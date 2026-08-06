920ed283d7c88f3538f5826aa5149fd00413fd67
Tatsuya Yamasaki <zakkikki1024@yahoo.co.jp>
2026-08-06 12:22:00 +0900

    fix: 書籍CRUDリクエストのカラム名修正

--- app/Http/Requests/UpdateBookRequest.php
+++ app/Http/Requests/UpdateBookRequest.php
@@ -35,8 +35,8 @@ class UpdateBookRequest extends FormRequest
             'publication_date' => ['required', 'date'],
             'description' => ['nullable', 'string', 'max:1000'],
             'image_url' => ['nullable', 'url', 'max:255'],
-            'genre_ids' => ['required', 'array', 'min:1'],
-            'genre_ids.*' => ['integer', 'exists:genres,id'],
+            'genres' => ['required', 'array', 'min:1'],
+            'genres.*' => ['integer', 'exists:genres,id'],
         ];
     }
 
